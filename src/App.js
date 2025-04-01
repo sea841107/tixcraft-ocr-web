e5d54c838e51f484b994f385d58b9b2b6720e140
dd <pkh_dd@photons.com.tw>
2025-04-01 17:56:37 +0800

    add steps

--- src/App.js
+++ src/App.js
@@ -48,7 +48,26 @@ function App() {
     };
 
     return (
-        <div className="d-flex justify-content-center align-items-center vh-100 bg-light">
+        <div className="d-flex align-items-center vh-100 bg-light" style={{ flexDirection: "column", marginTop: "50px" }}>
+            <div className="mb-4">
+                <ul>
+                    <li className="text-center list-group-item active" style={{ fontSize: "1.5rem" }}>Steps</li>
+                    <li className="list-group-item active" style={{ fontSize: "1rem" }}>
+                        1. Download the CAPTCHA images below.<br />
+                        <img src="https://i.ibb.co/pjjsL5PH/1742811006661.png" alt=""></img>&nbsp;
+                        <img src="https://i.ibb.co/NnnyGBzT/1742810855545-0.png" alt=""></img>&nbsp;
+                        <img src="https://i.ibb.co/cXQGdvtD/1741465416611-1.png" alt=""></img>
+                        <br />
+                        2. Or download it from <a href="https://tixcraft.com" target="_blank" rel="noopener noreferrer">Tixcraft</a>.<br />
+                        3. Upload the CAPTCHA image.<br />
+                        4. Click "Choose File" to select the image.<br />
+                        5. Click "Recognize" to get the result.<br />
+                        6. The result will be displayed below the button.<br />
+                        7. Make sure the image size is 120x100 pixels.<br />
+                        8. If the image size is incorrect, an error message will be shown.<br />
+                    </li>
+                </ul>
+            </div>
             <div className="card p-4 text-center shadow-lg" style={{ width: "350px" }}>
                 <input
                     type="file"
